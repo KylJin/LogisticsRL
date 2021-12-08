93a6d6ca72d89b85aeed5d2b69b50b47a5b27715
Kyle <61264165+KylJin@users.noreply.github.com>
2021-12-08 22:12:45 +0800

    Add examples for submission

---
+++ examples/rule/submission.py
@@ -0,0 +1,19 @@
+def my_controller(observation, action_space, is_act_continuous=False):
+    obs = observation['obs']
+    connections = observation['connected_player_index']
+    storage = obs['controlled_player_index']
+
+    action = []
+    for i, nbr in enumerate(connections):
+        high = action_space.high[i]
+        avg = (storage + obs[nbr]) / 2
+        if storage > avg:
+            act = storage - avg
+            if act <= high:
+                action.append(act)
+            else:
+                action.append(high)
+        else:
+            action.append(0)
+
+    return action
