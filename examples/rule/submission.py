59d9f2f2844452626e612ca7116f00e58ce20910
Kyle <61264165+KylJin@users.noreply.github.com>
2021-12-12 10:52:54 +0800

    Update submission.py
    fix error of uploading to Jidi platform.

--- examples/rule/submission.py
+++ examples/rule/submission.py
@@ -1,11 +1,11 @@
 def my_controller(observation, action_space, is_act_continuous=False):
     obs = observation['obs']
     connections = observation['connected_player_index']
-    storage = obs['controlled_player_index']
+    storage = obs[observation['controlled_player_index']]
 
     action = []
     for i, nbr in enumerate(connections):
-        high = action_space.high[i]
+        high = action_space[0].high[i]
         avg = (storage + obs[nbr]) / 2
         if storage > avg:
             act = storage - avg
@@ -16,4 +16,4 @@ def my_controller(observation, action_space, is_act_continuous=False):
         else:
             action.append(0)
 
-    return action
+    return [action]
